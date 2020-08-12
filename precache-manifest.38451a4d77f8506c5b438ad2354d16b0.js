cdb1bb4959534616dbb3d4cf26deac19e7901195
cre8rec <juliantmayes@gmail.com>
2020-08-11 21:38:04 -0700

    Updates

---
+++ precache-manifest.38451a4d77f8506c5b438ad2354d16b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018334713793196b0167f35b89946039",
+    "url": "/Job-Search/index.html"
+  },
+  {
+    "revision": "b520a9fbbbc799235f93",
+    "url": "/Job-Search/static/css/main.a505d55e.chunk.css"
+  },
+  {
+    "revision": "d96486157df706ff1a83",
+    "url": "/Job-Search/static/js/2.7ddc28b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Job-Search/static/js/2.7ddc28b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b520a9fbbbc799235f93",
+    "url": "/Job-Search/static/js/main.880edbdc.chunk.js"
+  },
+  {
+    "revision": "827dac7eb7c7bf53b8dc",
+    "url": "/Job-Search/static/js/runtime-main.0fb88646.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/Job-Search/static/media/bg-header-desktop.6e2f7196.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Job-Search/precache-manifest.38451a4d77f8506c5b438ad2354d16b0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Job-Search/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
